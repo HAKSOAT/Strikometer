631072109e6f7f51316c68bcf3b2d401799624c4
Habeeb Shopeju <shopejuh@gmail.com>
2018-11-28 09:01:06 +0100

    Configurations created

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__name__)))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+config = {
+'development': DevelopmentConfig,
+'production': ProductionConfig,
+'default': DevelopmentConfig
+}
