64d0126a024781ff8164742194e5f5d544545ef8
Habeeb Shopeju <shopejuh@gmail.com>
2018-11-28 14:11:45 +0100

    Imports changed to match changes in models

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
-from models import News
+from app import app
+import models
 
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, models.db)
 
 def make_shell_context():
-    return dict(app=app, db=db, News=News)
+    return dict(app=app, db=models.db, News=models.News)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
