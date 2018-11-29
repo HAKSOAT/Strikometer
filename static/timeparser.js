ae1d706c56ab45ccaef4bbff409e57d63856a6be
Habeeb Shopeju <shopejuh@gmail.com>
2018-11-29 01:33:41 +0100

    Time parser and counter added to javascript

---
+++ static/timeparser.js
@@ -0,0 +1,28 @@
+let flasktime = document.querySelectorAll(".flask-moment");
+for (let each = 0; each < flasktime.length; each++) {
+    flasktime[each].textContent = moment(flasktime[each].innerText).format('MMMM Do YYYY, h:mm:ss a');
+    flasktime[each].setAttribute("style","display: inline;");
+};
+
+let startDate = new Date("Nov 5, 2018 00:00:00").getTime();
+
+function counter() {
+
+    let currentTime = new Date().getTime();
+
+    let timeDifference = currentTime - startDate;
+
+    let months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30));
+    let days = Math.floor(timeDifference % (1000 * 60 * 60 * 24 * 30) / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    console.log(document.getElementsByClassName("counter"));
+
+    document.querySelector(".counter").textContent = months + "M " + days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+};
+
+setInterval(counter, 1000)
+
