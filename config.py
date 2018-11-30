88719cf54b5b2ff906a768171961192a3c9a95df
Habeeb Shopeju <shopejuh@gmail.com>
2018-11-30 13:45:26 +0100

    Add config settings for mailing system

--- config.py
+++ config.py
@@ -3,10 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-
+    SECRET_KEY = '36356e9db87405fd84fb18d0bd8380ff6ed47b74f1f578df'
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
+    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+    MAIL_DEFAULT_SENDER = os.environ["MAIL_DEFAULT_SENDER"]
+    MAIL_USE_SSL = True
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
 
 
 class DevelopmentConfig(Config):
